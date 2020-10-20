d48fde09422ed691d900f17891145c03effd2ec8
AczayacatlM <xchel.montoya.prog@gmail.com>
2020-10-20 00:33:45 -0500

    Subida la práctica de los arreglos

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -8,71 +8,62 @@ xchelxdx@comunidad.unam.mx
 
 #include<iostream>
 #include<cstring>
-#include<stdlib.h>
+#include<cstdlib>
 
 int main(){
 	
 	using namespace std;
 	
-	double x = 0, y = 0, z = 0;
-	int m = 0, n = 0, k = 0;
-	char op[2], rep[3], num1[50], num2[50];
+	char op, rep[3], num1[50], num2[50];
 	
+	cout << "Bienvenido a la Aczacalc-3000 \n\r Instrucciones de uso: \n\r"
+		<< "Para comenzar, ingrese un número, después teclee +, -, *, / o % dependiendo de la operación a realizar \n\r"
+		<< "Después ingrese un segundo número y se verificará si la operación puede o no realizarse. \n\r"
+		<< "De ser resoluble, se mostrará el resultado por pantalla y se permitirá repetir el programa. \n\r" << endl;
+	system("pause");
 	do{
 		system("cls");
-		cout << "Bienvenido a la Aczacalc-3000 \n\r Instrucciones de uso: \n\r"
-			<< "Para comenzar, ingrese un número, después teclee +, -, *, / o % dependiendo de la operación a realizar \n\r"
-			<< "Después ingrese un segundo número y se verificará si la operación puede o no realizarse. \n\r"
-			<< "De ser resoluble, se mostrará el resultado por pantalla y se permitirá repetir el programa. \n\r" << endl;
 		cout << "Ingresa el primer número: ";
 		cin.getline(num1, 50);
 		cin.ignore();
-		x = atof(num1);
+		double x = atof(num1);
 		if(x == 0 && strcmp(num1, "0") != 0){
 			cout << "La entrada del primer número es inválida" << endl;
 		}
 		else{
-			cout << "Escribe la operación a realizar: ";
-			cin.getline(op, 2);
+			cout << "El número ingresado es: " << x << endl; 
+			cout << "Escribe la operación a realizar: " <<endl;
+			op = cin.get();
 			cin.ignore();
-			if(strcmp(rep, "+") == 0 || strcmp(rep, "-") == 0 || strcmp(rep, "*") == 0 || strcmp(rep, "/") == 0 || strcmp(rep, "%") == 0){
-				cout << "El caracter de operación ingresado es inválido" << endl;
-			}
-			else{
-				m = atoi(num1);
-				if(strcmp(rep, "%")){
-					if(m == 0){
-						if(strcmp(num1, "0") != 0){
-							cout << "La operación es inválida, solo se puede realizar la operación módulo entre enteros" << endl;
-						}
-					}
+			if(op == 37 || op == 42 || op == 43 || op == 45 || op == 47){
+				cout << "La operación a realizar es: " << op << endl;
+				int m = atoi(num1);
+				if(op == 37 && m == 0 && strcmp(num1, "0") != 0){
+					cout << "La operación es inválida, solo se puede realizar la operación módulo entre enteros" << endl;
 				}
 				else{
 					cout << "Ingresa el segundo número: ";
 					cin.getline(num2, 50);
 					cin.ignore();
-					y = atof(num2);
+					double y = atof(num2);
 					if(y == 0){
 						if(strcmp(num2, "0") != 0){
 							cout << "La entrada del segundo número es inválida" << endl;
 						}
 					}
 					else{
-						n = atoi(num2);
-						if((strcmp(rep, "%") == 0 || strcmp(rep, "/") == 0) && n == 0 && strcmp(num2, "0") == 0){
+						int n = atoi(num2);
+						if((op == 37 || op == 47) && n == 0 && strcmp(num2, "0") == 0){
 							cout << "La operación es inválida, solo se puede realizar la división entre 0" << endl;
 						}
 						else{
-							if(strcmp(rep, "%") == 0){
-								if(n == 0){
-									if(strcmp(num1, "0") != 0){
-										cout << "La operación es inválida, solo se puede realizar la operación módulo entre enteros" << endl;
-									}
-								}
+							if(op == 37 && n == 0 && strcmp(num1, "0") != 0){
+								cout << "La operación es inválida, solo se puede realizar la operación módulo entre enteros" << endl;
 							}
 							else{
-								char aux = op[0];
-								switch(aux){
+								int k = 0;
+								double z = 0;
+								switch(op){
 								case '+':
 									z = x + y;
 									cout << "El resultado de la suma es: " << z << endl;
@@ -91,7 +82,7 @@ int main(){
 									break;
 								case '%':
 									k = m % n;
-									cout << "El resultado de la suma es: " << k << endl;
+									cout << "El resultado del módulo es: " << k << endl;
 									break;
 								default:
 									cout << "¡Felicidades, has encontrado un bug!" << endl;
@@ -102,11 +93,16 @@ int main(){
 					}
 				}
 			}
+			else{
+				cout << "El caracter de operación ingresado es inválido: " << op << endl;
+			}
 		}
 		cout << "Para realizar una nueva operación escribe Si: ";
 		cin.getline(rep, 3);
 		cin.ignore();
 		system("cls");
-		system("pause");
-	} while(strcmp(rep, "Si") == 0);
+	} while(strcmp(rep, "Si") == 0 || strcmp(rep, "si") == 0 || strcmp(rep, "SI") == 0 || strcmp(rep, "S") == 0 || strcmp(rep, "s") == 0);
+	
+	system("pause");
+	return 0;
 }
